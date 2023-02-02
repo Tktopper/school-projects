c3a327a4c6c9327820a67303a65f3a34d1470663
Tktopper <116019411+Tktopper@users.noreply.github.com>
2023-02-02 16:54:32 -0500

    Create AO311_rtl.v

---
+++ Digital Design: Theory and Practice/Project 1- Logic Element/AO311_rtl.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ns
+// Tanner Kleintop
+// Computer Engineering 270
+// September 7, 2022
+
+// hns12 08212017 - generate rtl model using boolean operators
+//
+module ao311_rtl (
+  input a,b,c,d,e, //listed inputs
+  output z //listed outputs
+  );
+  assign z = (a * b) * c & (d & e) ; //equation of my ao311 circuit
+endmodule
